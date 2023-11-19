6ea01b8dd356fff12e85b93f5beb9c0559f2ff15
KenLee <2533376457@qq.com>
2023-11-19 13:22:35 +0800

    列表投放到网页（尚未进行排版制图）

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import RadiationTable from "@/components/RadiationTable.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <RadiationTable />
   </main>
 </template>
 
