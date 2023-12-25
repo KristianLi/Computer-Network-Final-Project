300bd629566fd805f092c5432bd32b89fcfa67be
prophecy954 <154234980+prophecy954@users.noreply.github.com>
2023-12-25 19:35:01 +0800

    Update chinamap.vue

--- src/components/chinamap.vue
+++ src/components/chinamap.vue
@@ -83,14 +83,10 @@ export default {
             '#91cc75',//浅绿
             '#5470c6',//蓝色
             '#73c0de',//淡蓝
-<<<<<<< HEAD
-          ]
-=======
           ],
           textstyle:{
           color: '#fff'
           }
->>>>>>> 5fe5ef1 (first commit)
         },
         geo: {
           map: "china", // 表示中国地图
